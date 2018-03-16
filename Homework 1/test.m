5aadac3086b7d5b761cfbbcd6fb71ecc0113f2b5
Allar <allar.viinamae@hotmail.com>
2018-03-16 20:53:20 +0200

    Update

--- Homework 1/test.m
+++ Homework 1/test.m
@@ -30,7 +30,7 @@ dataset = [
 
 % should return Canberra Distance 0.272727
 % sensitive with short distances
-canberraDistance = MetricFunction.canberraDistance(rndVector1, rndVector2);
+% canberraDistance = MetricFunction.canberraDistance(rndVector1, rndVector2);
 
 % should return Mahalanobis Distance 3
 % mahal = MetricFunction.mahalanobisDistance(dataset, rndVector1);
@@ -60,36 +60,37 @@ canberraDistance = MetricFunction.canberraDistance(rndVector1, rndVector2);
 % end
 
 % DBSCAN
-% DBSCANStruct = load('dataset_test.mat');
-% DBSCANDataset = struct2dataset(DBSCANStruct);
-% DBSCANDataset = DBSCANDataset.DD;
-% 
-% DB = Point.empty;
-% for pointIDX = 1:length(DBSCANDataset)
-%     DB(pointIDX) = Point(DBSCANDataset(pointIDX, :), 0, pointIDX);
-% end
-% 
-% [labeledDB, clusters] = DBSCAN.execute(DB, 3, 6);
-% 
-% for clusterIDX = 1:clusters
-%     mask = arrayfun(@(x) x.label == clusterIDX, labeledDB);
-%     DBSCANClusterPoints = labeledDB(mask);
-%     
-%     arr = Point.toArray(DBSCANClusterPoints);
-%     scatter(arr(:,1),arr(:,2), 40, 'filled');
-%     labelNr = DBSCANClusterPoints(1).label;
-%     Legend{clusterIDX}=strcat('Label: ', num2str(labelNr));
-%     hold on;
-% end 
-% 
-% mask = arrayfun(@(x) x.label == -1, labeledDB);
-% DBSCANClusterPoints = labeledDB(mask);
-%     
-% arr = Point.toArray(DBSCANClusterPoints);
-% scatter(arr(:,1),arr(:,2), 40);
-% hold on;
-% 
-% legend(Legend);
-% grid on;
+DBSCANStruct = load('dataset_test.mat');
+DBSCANDataset = struct2dataset(DBSCANStruct);
+DBSCANDataset = DBSCANDataset.DD;
+
+DB = Point.empty;
+for pointIDX = 1:length(DBSCANDataset)
+    DB(pointIDX) = Point(DBSCANDataset(pointIDX, :), 0, pointIDX);
+end
+
+% As a rule of thumb, minPts = 2·dim can be used
+[labeledDB, clusters] = DBSCAN.execute(DB, 3, 4);
+
+for clusterIDX = 1:clusters
+    mask = arrayfun(@(x) x.label == clusterIDX, labeledDB);
+    DBSCANClusterPoints = labeledDB(mask);
+    
+    arr = Point.toArray(DBSCANClusterPoints);
+    scatter(arr(:,1),arr(:,2), 40, 'filled');
+    labelNr = DBSCANClusterPoints(1).label;
+    Legend{clusterIDX}=strcat('Label: ', num2str(labelNr));
+    hold on;
+end 
+
+mask = arrayfun(@(x) x.label == -1, labeledDB);
+DBSCANClusterPoints = labeledDB(mask);
+    
+arr = Point.toArray(DBSCANClusterPoints);
+scatter(arr(:,1),arr(:,2), 40);
+hold on;
+
+legend(Legend);
+grid on;
 
 
